6edd296d24723a55333bc9d803c562ceb3a19f3c
piotrek_kp <piotrek.kp@gmail.com>
2020-09-08 17:39:44 +0200

    javascript excercise completed

--- javascript/08092020.js
+++ javascript/08092020.js
@@ -1,11 +1,36 @@
-//1
-
-//2
-for (var i = 10; i >= 1; i--) {
-    console.log(i);
+// 1
+var test;
+function logType(arg) {
+    if (typeof arg === "undefined") {
+        console.log("undefined!");
+    } else if (arg === null) {
+        console.log("null!");
+    } else if (typeof arg === "number") {
+        if (isNaN(arg)) {
+            console.log("not a number!");
+        } else {
+            console.log("number!");
+        }
+    } else if (typeof arg === "string") {
+        console.log("string!");
+    } else if (typeof arg === "boolean") {
+        console.log("boolean!");
+    } else if (typeof arg === "bigint") {
+        console.log("bigint!");
+    } else if (typeof arg === "function") {
+        console.log("function!");
+    } else if (Array.isArray(arg) === true) {
+        console.log("array!");
+    } else if (typeof arg === "object") {
+        console.log("object!");
+    } else if (typeof arg === "symbol") {
+        console.log("I have no idea!");
+    }
 }
 
-//3
+logType(test);
+
+// 2
 var a = {
     Berlin: "Germany",
     Paris: "France",
@@ -16,4 +41,9 @@ var b = {};
 
 for (var key in a) {
     b[a[key]] = key;
+}
+
+// 3
+for (var i = 10; i >= 1; i--) {
+    console.log(i);
 }

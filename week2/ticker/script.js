c89b6b499593d6a94d92f75fbae0b926c749f13d
piotrek_kp <piotrek.kp@gmail.com>
2020-09-16 09:23:57 +0200

    ticker update

--- week2/ticker/script.js
+++ week2/ticker/script.js
@@ -1,31 +1,36 @@
 (function () {
     var headlines = document.getElementById("headlines");
-    var links = document.getElementsByTagName("a");
     var left = headlines.offsetLeft;
-    var animId = requestAnimationFrame(moveHeadlines)
-    // moveHeadlines();
+    var links = document.getElementsByTagName("a");
+    var animId = requestAnimationFrame(moveHeadlines);
+
+    moveHeadlines();
 
     function moveHeadlines() {
-        var left = left--;
-        console.log(left)
-        if (left <= -links[0].offsetWidth){
-            
-         animId = requestAnimationFrame(moveHeadlines)
-        
+        left--;
+        headlines.style.left = left + "px";
+
+        if (left <= -links[0].offsetWidth) {
+            left += links[0].offsetWidth;
+            headlines.appendChild([links[0]]);
+        }
+
+        animId = requestAnimationFrame(moveHeadlines);
     }
-        console.log("animId:", animId)
-    cancelAnimationFrame(animId);
 
-    console.log("links:", links)
+    animId = requestAnimationFrame(moveHeadlines);
 
-    //links value is array that conntains all anchor tags
-
-    links
-    links.addEventListener('mouseenter')
-
-    for(var i =0; i <links.length; i++){
-        links[i].addEventListener('mouseenter', function(){
-            console.log('mouseentered over a link')
-        })
+    cancelAnimationFrame(moveHeadlines);
+    for (var i = 0; i < links.length; i++) {
+        links[i].addEventListener("mouseenter", function (event) {
+            if (event.innerText === animId) {
+                links[i].style.color = "green";
+                links[i].style.textDecoration = "underline";
+            }
+        });
+        links[i].removeEventListener("mouseleave", function () {
+            links[i].style.color = "unset";
+            links[i].style.textDecoration = "none";
+        });
     }
 })();

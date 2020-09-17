bdd4920c9d819c51692d40a0f24f3848a99766d2
piotrek_kp <piotrek.kp@gmail.com>
2020-09-17 17:55:49 +0200

    Carousel exercise update

--- week2/carousel/script.js
+++ week2/carousel/script.js
@@ -4,26 +4,21 @@
     setTimeout(moveKitties, 3000);
 
     function moveKitties() {
-        if (i < 3) {
-            kitties[i].classList.remove("onscreen");
-            kitties[i].classList.add("offscreen-left");
-            //to pull the next kitty onscreen...
+        //to pull the next kitty onscreen...
+        kitties[i].classList.remove("onscreen");
+        kitties[i].classList.add("offscreen-left");
 
-            kitties[i + 1].classList.add("onscreen");
-            // to remove a kitty from the screen...
-            i++;
-            console.log(i);
-        } else {
-            kitties[i].classList.remove("onscreen");
-            kitties[i].classList.add("offscreen-left");
-            kitties[0].classList.add("onscreen");
+        //to go through kitties and make infinite loop
+        i++;
+        if (i === 4) {
             i = 0;
         }
+        // to remove a kitty from the screen...
+        kitties[i].classList.add("onscreen");
     }
 
     document.addEventListener("transitionend", function (event) {
         if (event.target.classList.value === "offscreen-left") {
-            console.log(event.target);
             event.target.classList.remove("offscreen-left");
         } else {
             return;

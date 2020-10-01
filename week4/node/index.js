78e7ccf922a1bd13172b48ed77f25980bd7e7696
piotrek_kp <piotrek.kp@gmail.com>
2020-10-01 16:36:24 +0200

    node thursday update

---
+++ week4/node/index.js
@@ -0,0 +1,81 @@
+var s = require("./script");
+const handlebars = require("handlebars");
+
+//when i install something from NPM i dont neet to specify location
+
+const tmpl = handlebars.compile("Hello, {{name}} ");
+
+const str = tmpl({
+    name: "Kitty",
+});
+
+console.log(str);
+
+//you can require the  same file multiple times
+// Url parse
+//querystring
+//modules that are built in in node
+
+console.log(s);
+
+// __dirname - folder that contains index.js
+// __filename - folder + filename
+
+// console.log(module);
+
+// Way to export a data to use by other files
+// module.exports.doSomething = function() {
+// console.log("I am doing something")
+// }
+
+// console.log(exports == module.exports);
+
+// inside modules, keyword this refers to exports
+
+//process module is automaticly required, always avaiable in the global scope
+//property called env
+//variables you may use to make avaiable for your running process
+//common you put secret things you dont want to put into git repository
+
+//console.log(process.USER)
+console.log(Object.keys(process).sort());
+console.log(process.argv);
+process[argv2]
+require url module,
+parse,
+get Objectlog strings regarding the properties
+if exist / null, you shoulr parse the query string. 
+Loop object and log name and value every query string object
+listing things to start a process
+
+// You can write your own terminal programs by this
+
+callbacks,
+
+event emitters
+
+setTimout((function){
+
+}), 4500)
+
+getData(function(err, data){
+    if (err) {
+        handle the error
+    } else {
+        do something wit data
+    }
+}
+const data = getData(function(data){
+
+});
+
+event emitters 
+run this function whenever that happends
+
+built in module evebts
+event emitter
+function emmit, 
+funciton onabort
+process.on('beforeExit', function(){
+    console.log("about to exit")
+})
---
+++ week4/node/script.js
@@ -0,0 +1,6 @@
+let piotr = "lol";
+
+exports.piotrName = piotr;
+exports.piotrLastName = "Majtyka";
+
+//way to export

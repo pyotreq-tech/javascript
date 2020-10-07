bffd1369c4074ab5e6330f9c06116ce5a82ec12e
piotrek_kp <piotrek.kp@gmail.com>
2020-10-07 10:39:11 +0200

    Cluster update

---
+++ week5/portfolio/cluster.js
@@ -0,0 +1,17 @@
+const cluster = require("cluster");
+const os = require("os");
+
+const numCpus = os.cpus().length;
+
+cluster.setupMaster({
+    exec: __dirname + "index.js",
+});
+
+for (let i = 0; i < numCpus; i++) {
+    cluster.fork();
+}
+
+cluster.on("exit", (worker) => {
+    console.log("this worker has died :(", worker.process.pid);
+    cluster.fork();
+});

09f2af52baca00a907b942a9b21faee4dd3566fb
piotrek_kp <piotrek.kp@gmail.com>
2020-10-07 18:34:12 +0200

    Cluster exercise

--- week5/portfolio/cluster.js
+++ week5/portfolio/cluster.js
@@ -4,7 +4,7 @@ const os = require("os");
 const numCpus = os.cpus().length;
 
 cluster.setupMaster({
-    exec: __dirname + "index.js",
+    exec: __dirname + "/index.js",
 });
 
 for (let i = 0; i < numCpus; i++) {

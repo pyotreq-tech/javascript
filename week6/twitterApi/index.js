82795286025d289b0256a38f1435672c2c375395
piotrek_kp <piotrek.kp@gmail.com>
2020-10-13 18:50:58 +0200

    TwitterApi refactored with Promises

--- week6/twitterApi/index.js
+++ week6/twitterApi/index.js
@@ -1,3 +1,65 @@
+// const express = require("express");
+// const app = express();
+
+// const { getToken, getTweets, filterTweets } = require("./ticker.js");
+
+// app.use(express.static("ticker"));
+
+// app.get("/links.json", (req, res) => {
+//     //call with promise syntax
+//     getToken(function (err, barerToken) {
+//         if (err) {
+//             console.log("Error in getToken: ", err);
+//             return;
+//         }
+//         //call with promise syntax
+//         getTweets(barerToken, function (err, tweets) {
+//             if (err) {
+//                 console.log("Error in tweets: ", err);
+//                 return;
+//             }
+
+//             const filteredTweets = filterTweets(tweets);
+//             res.json(filteredTweets);
+//         });
+//     });
+// });
+
+// app.listen(8080, () => {
+//     console.log("Server is waiting for the tweets...");
+// });
+
+// FIRST WE NEED TO PROMISIFY OUR TWITTER
+
+// const express = require("express");
+// const app = express();
+
+// const { getToken, getTweets, filterTweets } = require("./ticker.js");
+
+// app.use(express.static("ticker"));
+
+// app.get("/links.json", (req, res) => {
+//     //call with promise syntax
+//     getToken()
+//         .then((token) => {
+//             return getTweets(token, "Drake").then((drakeTweets) => {
+//                 getTweets(token, "papinadepalma").then((papinaTweets) => {
+//                     getTweets(token, "MarsCuriosity").then((marsTweets) => {
+//                         // combine tweets/filter tweets/send them back
+//                     });
+//                 });
+//             });
+//         })
+//         .catch((err) => {
+//             console.log("err in catch: ", err);
+//             res.sendStatus(500);
+//         });
+// });
+
+// app.listen(8080, () => {
+//     console.log("Server is waiting for the tweets...");
+// });
+
 const express = require("express");
 const app = express();
 
@@ -6,22 +68,49 @@ const { getToken, getTweets, filterTweets } = require("./ticker.js");
 app.use(express.static("ticker"));
 
 app.get("/links.json", (req, res) => {
-    getToken(function (err, barerToken) {
-        if (err) {
-            console.log("Error in getToken: ", err);
-            return;
-        }
+    //call with promise syntax
+    getToken()
+        .then((token) => {
+            // takes single argument - array of promises
+            Promise.all([
+                // no matter when it comes back, the order stays the same in results
+                getTweets(token, "MTV"),
+                getTweets(token, "papinadepalma"),
+                getTweets(token, "MarsCuriosity"),
+            ])
+                .then((results) => {
+                    // [[drakeTweets], [papinaTweets], [marsTweets]];
+                    // const drake = results[0];
+                    // const papina = results[1];
+                    // const mars = results[2];
+                    // const { drake, papina, mars } = results;
+                    // const mergedResults = drake.concat(papina, mars);
+                    // const mergedResults = [...drake, ...papina, ...mars];
+                    const mergedResults = [
+                        ...results[0],
+                        ...results[1],
+                        ...results[2],
+                    ];
 
-        getTweets(barerToken, function (err, tweets) {
-            if (err) {
-                console.log("Error in tweets: ", err);
-                return;
-            }
+                    const sortedTweets = mergedResults.sort((a, b) => {
+                        return new Date(b.created_at) - new Date(a.created_at);
+                    });
 
-            const filteredTweets = filterTweets(tweets);
-            res.json(filteredTweets);
+                    // console.log(sortedTweets);
+
+                    const filteredTweets = filterTweets(sortedTweets);
+                    res.json(filteredTweets);
+                })
+                // if all 3 resolved - this runs
+                .catch((err) => {
+                    console.log("err in promise all: ", err);
+                });
+            // if any single promise doesn't resolve as expected - here runs
+        })
+        .catch((err) => {
+            console.log("err in catch: ", err);
+            res.sendStatus(500);
         });
-    });
 });
 
 app.listen(8080, () => {
--- week6/twitterApi/ticker.js
+++ week6/twitterApi/ticker.js
@@ -1,77 +1,81 @@
 const { Key, Secret } = require("./secrets.json");
 const https = require("https");
 
-exports.getToken = function (callback) {
-    let creds = `${Key}:${Secret}`;
-    let encodedCreds = Buffer.from(creds).toString("base64");
-    const options = {
-        method: "POST",
-        host: "api.twitter.com",
-        path: "/oauth2/token",
-        headers: {
-            Authorization: `Basic ${encodedCreds}`,
-            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-        },
-    };
+exports.getToken = () => {
+    return new Promise((resolve, reject) => {
+        let creds = `${Key}:${Secret}`;
+        let encodedCreds = Buffer.from(creds).toString("base64");
+        const options = {
+            method: "POST",
+            host: "api.twitter.com",
+            path: "/oauth2/token",
+            headers: {
+                Authorization: `Basic ${encodedCreds}`,
+                "Content-Type":
+                    "application/x-www-form-urlencoded;charset=UTF-8",
+            },
+        };
 
-    function cb(response) {
-        if (response.statusCode != 200) {
-            console.log(
-                "Something went wrong in getToken: ",
-                response.statusCode
-            );
-            callback(response.statusCode);
-            return;
+        function cb(response) {
+            if (response.statusCode != 200) {
+                console.log(
+                    "Something went wrong in getToken: ",
+                    response.statusCode
+                );
+                reject(response.statusCode);
+            }
+            let body = "";
+            response.on("data", function (chunk) {
+                body += chunk;
+            });
+            response.on("end", function () {
+                let parsedBody = JSON.parse(body);
+                resolve(parsedBody.access_token);
+            });
         }
-        let body = "";
-        response.on("data", function (chunk) {
-            body += chunk;
-        });
-        response.on("end", function () {
-            let parsedBody = JSON.parse(body);
-            callback(null, parsedBody.access_token);
-        });
-    }
-    const req = https.request(options, cb);
-    req.end("grant_type=client_credentials");
+        const req = https.request(options, cb);
+        req.end("grant_type=client_credentials");
+    });
 };
-exports.getTweets = function (bearerToken, callback) {
-    const options = {
-        method: "GET",
-        host: "api.twitter.com",
-        path:
-            "/1.1/statuses/user_timeline.json?count=20&screen_name=BBCWorld&tweet_mode=extended",
 
-        headers: {
-            Authorization: `Bearer ${bearerToken}`,
-        },
-    };
+exports.getTweets = function (bearerToken, account) {
+    return new Promise((resolve, reject) => {
+        const options = {
+            method: "GET",
+            host: "api.twitter.com",
+            path: `/1.1/statuses/user_timeline.json?count=20&screen_name=${account}&tweet_mode=extended`,
 
-    function cb(response) {
-        if (response.statusCode != 200) {
-            console.log(
-                "Something went wrong in Tweets: ",
-                response.statusCode
-            );
-            callback(response.statusCode);
-            return;
+            headers: {
+                Authorization: `Bearer ${bearerToken}`,
+            },
+        };
+
+        function cb(response) {
+            if (response.statusCode != 200) {
+                console.log(
+                    "Something went wrong in Tweets: ",
+                    response.statusCode
+                );
+                reject(response.statusCode);
+            }
+            let body = "";
+            response.on("data", function (chunk) {
+                body += chunk;
+            });
+            response.on("end", function () {
+                let parsedBody = JSON.parse(body);
+
+                resolve(parsedBody);
+            });
         }
-        let body = "";
-        response.on("data", function (chunk) {
-            body += chunk;
-        });
-        response.on("end", function () {
-            let parsedBody = JSON.parse(body);
 
-            callback(null, parsedBody);
-        });
-    }
-
-    const req = https.request(options, cb);
-    req.end();
+        const req = https.request(options, cb);
+        req.end();
+    });
 };
 
 exports.filterTweets = function (tweets) {
+    //here we need to add who made a tweet, we need to append that to text
     let newObject = [];
     function check(array) {
         return array.entities.urls.length === 1;
@@ -80,10 +84,10 @@ exports.filterTweets = function (tweets) {
 
     newArray.forEach((arg) => {
         newObject.push({
-            text: arg["full_text"].split("http")[0],
+            text: `"${arg.user.name}": ${arg["full_text"].split("http")[0]}`,
             href: arg.entities.urls[0].url,
         });
-        console.log(arg.entities.urls);
+        // console.log(arg.entities.urls);
     });
     return newObject;
 };

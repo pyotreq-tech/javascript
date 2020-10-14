3d6d6d1f29e4de471f68394bc970a3bb99168ca7
piotrek_kp <piotrek.kp@gmail.com>
2020-10-14 21:58:33 +0200

    fn exercise completed

--- week6/jest-testing-encounter/testing/exercises/3/fn.js
+++ week6/jest-testing-encounter/testing/exercises/3/fn.js
@@ -1,2 +1,13 @@
-module.exports = function fn() {
+module.exports = function fn(arg) {
+    if (typeof arg === "string") {
+        return [...arg].reverse().join("");
+    } else if (typeof arg !== "string" && Array.isArray(arg) === false) {
+        return null;
+    } else if (Array.isArray(arg)) {
+        let newArr = [];
+        arg.forEach((a) => {
+            newArr.push(fn(a));
+        });
+        return newArr;
+    }
 };
